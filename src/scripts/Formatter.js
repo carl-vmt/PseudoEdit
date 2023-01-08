172a36984ccb64327b3806c2e8ee14b1fafc3885
Carl M <mutius.carl@gmail.com>
2023-01-08 22:05:40 +0100

    Format using prettier

--- src/scripts/Formatter.js
+++ src/scripts/Formatter.js
@@ -67,10 +67,7 @@ function formatLine(line) {
     }
 
     if (lastType === "keyword") {
-      if (
-        /^[a-zA-Z0-9_]+$/.test(el) &&
-        !/^[0-9]+$/.test(el)
-      ) {
+      if (/^[a-zA-Z0-9_]+$/.test(el) && !/^[0-9]+$/.test(el)) {
         if (type === "default" || type === "number") {
           elements.push(createArray("declarator", el));
           continue;
